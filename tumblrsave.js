49eb44a9c63e7a19944ac5b31c5969ec939725fb
Lars Boegild Thomsen <lth@netcompartner.com>
2018-12-10 10:55:21 +0800

    Fetching

--- tumblrsave.js
+++ tumblrsave.js
@@ -11,38 +11,48 @@ const package = require(__dirname + "/package.json");
 
 const optionDefs = [
     {
-        name: "verbose", 
-        alias: "v", 
-        type: Boolean, 
+        name: "verbose",
+        alias: "v",
+        type: Boolean,
         defaultValue: false
-    }, 
+    },
     {
-        name: "src", 
-        alias: "s", 
+        name: "src",
+        alias: "s",
         type: String
-    }, 
+    },
     {
-        name: "help", 
-        alias: "h", 
-        type: Boolean, 
+        name: "help",
+        alias: "h",
+        type: Boolean,
         defaultValue: false
-    }, 
+    },
     {
-        name: "number", 
-        alias: "n", 
-        type: Number, 
+        name: "number",
+        alias: "n",
+        type: Number,
         defaultValue: 50
+    },
+    {
+        name: "output",
+        alias: "o",
+        type: String
     }
 ];
 
-async function doRun() {
+/**
+ * Fetch stuff from Tumblr
+ */
+async function doFetch() {
+
+    options.verbose && console.error("Fetching from Tumblr");
 
     var baseUrl = options.src + "/api/read?num=" + options.number;
 
     var blog = {
         posts: []
     };
-    
+
     /**
      * Let's fetch 
      */
@@ -53,7 +63,7 @@ async function doRun() {
 
         var rawResponse;
         try {
-            options.verbose && console.log("Requesting: %j", requestUrl);
+            options.verbose && console.error("Requesting: %j", requestUrl);
             rawResponse = await request(requestUrl);
         } catch (err) {
             console.error("Got request error: %j", err);
@@ -73,44 +83,78 @@ async function doRun() {
          */
         if (total === 1) {
             total = parseInt(jsonResponse.tumblr.posts[0].$.total);
-            options.verbose && console.log("Total set to ", total);
+            options.verbose && console.error("Total set to ", total);
         }
 
         /**
          * Update blog title if needed
          */
         if (!blog.title) {
-            blog.title = tumblr.tumblelog.$.title;
+            blog.title = jsonResponse.tumblr.tumblelog[0].$.title;
         }
 
         /**
          * Update blog name if needed
          */
         if (!blog.name) {
-            blog.name = tumblr.tumblelog.$.name;
+            blog.name = jsonResponse.tumblr.tumblelog[0].$.name;
         }
 
         // Go through posts
-        options.verbose && console.log("Going through " + jsonResponse.tumblr.posts[0].post.length + " posts");
+        options.verbose && console.error("Going through " + jsonResponse.tumblr.posts[0].post.length + " posts");
 
         for (var postNumber = 0, len = jsonResponse.tumblr.posts[0].post.length; postNumber < len; ++postNumber) {
             var origPost = jsonResponse.tumblr.posts[0].post[postNumber];
+            var post = {};
 
-            console.log("Original post: ", JSON.stringify(origPost, null, 2));
+            post.tumblrId = origPost.$.id;
+            post.postType = origPost.$.type;
+            post.tumblrUrl = origPost.$.url;
+            post.tumblrSlug = origPost.$.slug;
+
+            blog.posts.push(post);
+
+            //console.error("Original post: ", JSON.stringify(origPost, null, 2));
 
         }
 
         start += options.number;
 
     }
+
+    options.verbose && console.error("Done Fetching from Tumblr");
+
+    return blog;
+
+}
+
+async function doStdout(blog) {
+    console.log(JSON.stringify(blog, null, 4));
+}
+
+async function doRun() {
+    options.verbose && console.error("Starting Import");
+
+    try {
+        var blog = await doFetch();
+
+        if (options.output === "stdout") {
+            await doStdout(blog);
+        } else {
+            console.error("Unknown output");
+        }
+    } catch (error) {
+        console.error("Failed!");
+    }
+
 }
 
 var options = commandLineArgs(optionDefs);
 
-options.verbose && console.log("Command line options: %j", options);
+options.verbose && console.error("Command line options: %j", options);
 
 if (options.help) {
-    console.log("Usage: tumblrsave [-v|--verbose] [-s|--src tumblr_url]")
+    console.error("Usage: tumblrsave [-v|--verbose] [-s|--src tumblr_url]")
 } else {
     doRun();
 }
